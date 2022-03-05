9a0cb0900b960df4d2dbeb46748186d66791a6c7
Dieter Vrancken <dieter.v91@gmail.com>
2022-03-05 22:01:00 +0100

    add and remove tasks

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,24 +1,73 @@
 <template>
-  <ul v-for="task in tasks" :key="task">
-      <li >{{task}}</li>
+  <ul >
+      <li v-for="(task, index) in tasks" :key="task">{{task}}  <font-awesome-icon @click="deleteTask(index)" :icon="['fas', 'trash']" class="red" />
+
+ </li>
   </ul>
   <form>
       <input type="text" v-model="newTaskName">
-      <button @click="addNewTask(newTaskName)">Add task</button>
+      <button @click.prevent="addNewTask()">Add task</button>
   </form>
 </template>
 
 <script>
+
 export default {
     data() {
         return {
             newTaskName: '',
             tasks: []
         }
+    },
+    methods: {
+        addNewTask() {
+            if(this.newTaskName != '') {
+                this.tasks.push(this.newTaskName)
+                this.newTaskName = ''
+            }
+        },
+        deleteTask(task) {
+            console.log(task);
+            this.tasks.splice(task, 1)
+        }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+    input, button {
+        border: none;
+        border-bottom: 1px solid gray;
+        padding: 10px;
+    }
+    button {
+        cursor: pointer;
+        color: white;
+        background: purple;
+    }
+    li {
+        list-style: none;
+        color: white;
+        font-weight: bold;
+        letter-spacing: 1px;
+        padding: .2rem .2rem;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    ul {
+        margin: auto;
+        padding:0;
+        width: 75%;
+    }
+    li:nth-child(odd) {
+        background: purple;
+    }
+    li:nth-child(even) {
+        background: white;
+        color: black;
+    }
+    .red{
+        color: black;
+    }
 </style>
